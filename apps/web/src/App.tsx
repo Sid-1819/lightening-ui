8c2de282699611c1c07988946b04d9d8fdf2b82d
Sid-1819 <siddhesh.shirdhankar18@gmail.com>
2025-02-04 23:59:22 +0530

    feat:setup

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,15 @@
+
+import "./App.css";
+
+function App() {
+
+	return (
+	
+			<p className="text-red-500">
+				Click on the Rspack and React logos to learn more
+			</p>
+	
+	);
+}
+
+export default App;
